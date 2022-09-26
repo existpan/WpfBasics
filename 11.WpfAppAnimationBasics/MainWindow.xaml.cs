b22beb36515b9e1c6d4900f2f22fc34f6e950547
Peter-Pan <Peter-Pan@PC-20220903JJSA>
2022-09-26 15:06:04 +0800

    Animation Basics.

--- 11.WpfAppAnimationBasics/MainWindow.xaml.cs
+++ 11.WpfAppAnimationBasics/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,26 @@ namespace _11.WpfAppAnimationBasics
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = this.button.Width;
+            doubleAnimation.To = this.button.Width + 30;
+            doubleAnimation.Duration =  TimeSpan.FromSeconds(0.8);
+            doubleAnimation.AutoReverse = false;
+            doubleAnimation.RepeatBehavior = new RepeatBehavior(2);
+
+            doubleAnimation.Completed += DoubleAnimation_Completed;
+
+            button.BeginAnimation(Button.WidthProperty, doubleAnimation);
+
+        }
+
+        private void DoubleAnimation_Completed(object sender, EventArgs e)
+        {
+            button.Content = "Animation Is Completed";
         }
     }
 }
