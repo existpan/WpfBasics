0a1087bb3a2c0f1cb892924e69c48f0a1cf8bcdf
Peter-Pan <Peter-Pan@PC-20220903JJSA>
2022-09-26 14:39:36 +0800

    资源基础ResourceBase。

---
+++ 10.WpfAppResourceBase/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10.WpfAppResourceBase
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["SolidColor"] = new SolidColorBrush(Colors.Aquamarine);
+            var solidColor = App.Current.FindResource("SolidColor");
+            var buttonStyle = App.Current.FindResource("DefaultButtonStyle");
+        }
+    }
+}
