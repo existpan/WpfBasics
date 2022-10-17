936ebc38bc656c0819b26c1c7c68d8088c57161b
Peter-Pan <Peter-Pan@PC-20220903JJSA>
2022-10-17 13:53:39 +0800

    Save

---
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        public ViewAViewModel()
+        {
+            title = "Hello";
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
