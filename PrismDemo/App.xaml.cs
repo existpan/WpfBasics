1127803a040246ff3da440bd48a37627efe9d4e8
Peter-Pan <Peter-Pan@PC-20220903JJSA>
2022-10-17 14:22:05 +0800

    基于Prism完成WPFDemo。

---
+++ PrismDemo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
