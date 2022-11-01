9ec01e7d299b1f53eb76c2f72336d3a15418dc7f
Peter-Pan <Peter-Pan@PC-20220903JJSA>
2022-11-01 08:41:19 +0800

    Save

---
+++ WpfAppConversationServicesModuleA/ViewAProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppConversationServicesModuleA
+{
+    public class ViewAProfile
+    {
+
+    }
+}
---
+++ WpfAppPrismRegion/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfAppPrismRegion.Views;
+
+namespace WpfAppPrismRegion.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public DelegateCommand<string> OpenCommand { get; set; }
+
+        private object body;
+
+        public object Body
+        {
+            get { return body; }
+            set
+            {
+                body = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            this.OpenCommand = new DelegateCommand<string>(Open);
+        }
+
+        private void Open(string obj)
+        {
+            switch (obj)
+            {
+                case "ViewA":
+                    Body = new ViewA();
+                    break;
+                case "ViewB":
+                    Body = new ViewB();
+                    break;
+                case "ViewC":
+                    Body = new ViewC();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
